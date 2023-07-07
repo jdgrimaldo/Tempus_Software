7e881d586e28b6f132ecdc9f402461f49fe6f66f
StudiosDanilIs <dalviarezhernandez@gmail.com>
2023-07-07 16:07:22 -0400

    nueva funcion muy util
    solo se puede ver en inicio ya que es donde hay mas contenido pero sirve para todos los lugares menos login

--- Tempus Software/js/acciones.js
+++ Tempus Software/js/acciones.js
@@ -1,14 +1,17 @@
-let menuToggleIcon = document.querySelector('.menu-toggle i');
-let menu = document.getElementById('menu');
-let menuToggle = document.querySelector('.menu-toggle');
-
-menuToggle.addEventListener('click', e=>{
-    menu.classList.toggle('show');
-
-    if(menu.classList.contains('show')){
-        menuToggleIcon.setAttribute('class', 'fa fa-times');
-    }else{
-        menuToggleIcon.setAttribute('class', 'fa fa-bars');
+window.onscroll = function(){
+    console.log(document.documentElement.scrollTop);
+    if(document.documentElement.scrollTop > 100) {
+      document.querySelector('.go-top-container').classList.add('show');
+       
     }
-
-});
+    else{
+      document.querySelector('.go-top-container').classList.remove('show');
+    }
+  }
+   
+  document.querySelector('.go-top-container').addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });

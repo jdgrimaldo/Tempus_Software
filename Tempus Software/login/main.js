02c7840d4cbf02e6ee131904239ea58fdc680116
jdgrimaldo <jose.danielgrimaldo@gmail.com>
2023-07-14 23:01:00 +0100

    .

--- Tempus Software/login/main.js
+++ Tempus Software/login/main.js
@@ -102,15 +102,15 @@ const validarCampo = (expresion, input, campo) =>{
     if(expresion.nombre.test(input.value)){
         document.getElementById(`grupo__${campo}`).classList.remove("formulario__grupo-incorrecto");
         document.getElementById(`grupo__${campo}`).classList.add("formulario__grupo-correcto");
-        document.querySelector(`#grupo__${campo} i`).classList.remove("fa-circle-xmark");
-        document.querySelector(`#grupo__${campo} i`).classList.add("fa-circle-check");
+        document.querySelector(`#grupo__${campo} i`).classList.remove("fa-square-xmark");
+        document.querySelector(`#grupo__${campo} i`).classList.add("fa-square-check");
         document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove("formulario__input-error-activo");
         campos[campo] = true;
     } else {
         document.getElementById(`grupo__${campo}`).classList.add("formulario__grupo-incorrecto");
         document.getElementById(`grupo__${campo}`).classList.remove("formulario__grupo-incorrecto");
-        document.querySelector(`#grupo__${campo} i`).classList.add("fa-circle-xmark");
-        document.querySelector(`#grupo__${campo} i`).classList.remove("fa-circle-check");
+        document.querySelector(`#grupo__${campo} i`).classList.add("fa-fa-square-xmark");
+        document.querySelector(`#grupo__${campo} i`).classList.remove("fa-square-check");
         document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add("formulario__input-error-activo");
         campos[campo] = false;
     }

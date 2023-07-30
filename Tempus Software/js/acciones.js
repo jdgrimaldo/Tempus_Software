4b99ee95792ab1228a15825d1b27d001ebc2d922
jdgrimaldo <jose.danielgrimaldo@gmail.com>
2023-07-30 17:03:10 +0100

    xyz

--- Tempus Software/js/acciones.js
+++ Tempus Software/js/acciones.js
@@ -13,33 +13,6 @@
     });
 })();
 
-
-/*Salir de Sesion del Sistema*/
-const user = JSON.parse(localStorage.getItem('login_success')) || false
-if(!user){
-}
-
-const logout = document.querySelector('#logout')
-
-logout.addEventListener('click', ()=>{
-    alert('Hasta pronto!')
-    localStorage.removeItem('login_success')
-})
-
-const login_success = JSON.parse(localStorage.getItem('login_success'))
-if (login_success) {
-const mensajeBienvenida = document.querySelector('#mensaje-bienvenida')
-mensajeBienvenida.innerText = `Bienvenido, ${login_success.usuario}!`;
-}
-
-const logoutButton = document.querySelector('#logout')
-
-logoutButton.addEventListener('click', () => {
-    localStorage.removeItem('login_success')
-    location.reload()
-})
-
-
 /*Catalogo De Laptos y Productos*/
 
 function toggleDescripcion(id) {

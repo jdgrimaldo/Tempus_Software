4544d78da491a8f176eca3292b623af0b6c8f6f2
StudiosDanilIs <dalviarezhernandez@gmail.com>
2023-10-13 07:29:14 -0400

    pagina principal tempus finalizada con exito

--- Tempus Software/js/acciones y animaciones.js
+++ Tempus Software/js/acciones y animaciones.js
@@ -2,26 +2,26 @@
 /*--Acciones para el Catalogo de Productos--*/
 /* Aparicion de Info/Texto a los Productos del Catalogo */
 function toggleDescripcion(id) {
-    var descripcion = document.querySelector(`#${id}`);
-    descripcion.classList.toggle('visible');
+  var descripcion = document.querySelector(`#${id}`);
+  descripcion.classList.toggle('visible');
 }
 
 
 /*Boton para desplegar para arriba*/
-window.onscroll = function(){
-    console.log(document.documentElement.scrollTop);
-    if(document.documentElement.scrollTop > 100) {
-      document.querySelector('.go-top-container').classList.add('show');
-       
-    }
-    else{
-      document.querySelector('.go-top-container').classList.remove('show');
-    }
+window.onscroll = function () {
+  console.log(document.documentElement.scrollTop);
+  if (document.documentElement.scrollTop > 100) {
+    document.querySelector('.go-top-container').classList.add('show');
+
   }
-   
-  document.querySelector('.go-top-container').addEventListener('click', () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
+  else {
+    document.querySelector('.go-top-container').classList.remove('show');
+  }
+}
+
+document.querySelector('.go-top-container').addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
   });
+});

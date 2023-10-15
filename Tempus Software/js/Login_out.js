4efce55c3fe4f435f5f546b0b59633d2b85ba243
Daniel Gimaldo <135259330+jdgrimaldo@users.noreply.github.com>
2023-10-15 13:51:10 +0100

    Función cerrar sesión
    Modifiqué la opción de cerrar sesión  para que no salga cuando no se haya iniciado sesión

--- Tempus Software/js/Login_out.js
+++ Tempus Software/js/Login_out.js
@@ -1,31 +1,13 @@
-/*Salir de Sesion del Sistema*/
-
 //Obtener el usuario almacenado en la base de datos local:
 const user = JSON.parse(localStorage.getItem('login_success')) || false
 
-//Comprobar si hay un usuario almacenado en la base de datos local, y si no lo hay, realizar alguna acción. 
-//En este caso, el código no hace nada si no hay un usuario almacenado:
+//Comprobar si hay un usuario almacenado en la base de datos local, y si no lo hay, ocultar el botón de cerrar sesión:
 if(!user){
-} // si no hay un usuario almacenado, no se hace nada
+  const logout = document.querySelector('#logout')
+  logout.style.display = 'none'
+} 
 
 //Obtener el botón de cerrar sesión y asignar un evento click que borra el usuario almacenado de la base de datos local:
-const logout = document.querySelector('#logout')
-
-logout.addEventListener('click', ()=>{
-    alert('Hasta pronto!')
-    localStorage.removeItem('login_success')
-})
-
-//Obtener el usuario almacenado en la base de datos local y mostrar un mensaje de bienvenida en la página de 
-//inicio si el usuario ha iniciado sesión correctamente:
-const login_success = JSON.parse(localStorage.getItem('login_success'))
-if (login_success) {
-const mensajeBienvenida = document.querySelector('#mensaje-bienvenida')
-mensajeBienvenida.innerText = `${login_success.usuario}!`;
-}
-
-//Obtener el botón de cerrar sesión y asignar un evento click que borra el usuario almacenado de la base de 
-//datos local, recarga la página y redirige al usuario a la página de inicio:
 const logoutButton = document.querySelector('#logout')
 
 logoutButton.addEventListener('click', () => {
@@ -33,3 +15,11 @@ logoutButton.addEventListener('click', () => {
     location.reload()
     window.location.href = 'index.html'
 })
+
+//Obtener el usuario almacenado en la base de datos local y mostrar un mensaje de bienvenida en la página de 
+//inicio si el usuario ha iniciado sesión correctamente:
+const login_success = JSON.parse(localStorage.getItem('login_success'))
+if (login_success) {
+  const mensajeBienvenida = document.querySelector('#mensaje-bienvenida')
+  mensajeBienvenida.innerText = `${login_success.usuario}!`;
+}
